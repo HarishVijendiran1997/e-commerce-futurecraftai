95fcd53b131bfb13488bbd1480262301734b27d7
HarishVijendiran97 <harish.vijendiran@gmail.com>
2025-04-26 23:02:45 +0530

    fix: params 10

--- src/lib/fetchProductDetails.ts
+++ src/lib/fetchProductDetails.ts
@@ -1,6 +1,6 @@
 import { Product } from "@/types/product";
 
-export default async function fetchProductById(id: string): Promise<Product> {
+export default async function fetchProductById(id: string) {
   const res = await fetch(`https://fakestoreapi.com/products/${id}`);
   // Single product: https://fakestoreapi.com/products/{id} - api requirement 4
   if (!res.ok) throw new Error("Failed to fetch product");
